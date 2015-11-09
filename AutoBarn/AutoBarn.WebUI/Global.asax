f74e8e1de79cb3c5645b7229ded4a1dc8dc11865
anthonyjoanes <anthonyjoanes@gmail.com>
2015-11-09 22:39:53 +0000

    initial commit

---
+++ AutoBarn/AutoBarn.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutoBarn.WebUI.MvcApplication" Language="C#" %>
---
+++ AutoBarn/AutoBarn.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutoBarn.WebUI.Startup))]
+namespace AutoBarn.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
