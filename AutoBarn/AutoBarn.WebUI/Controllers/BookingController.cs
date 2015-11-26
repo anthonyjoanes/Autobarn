98861c35e122a11ffa19997f0a0163f615ddb0c8
anthonyjoanes <anthonyjoanes@gmail.com>
2015-11-26 22:18:20 +0000

    more styling and cascading dropdown lists

--- AutoBarn/AutoBarn.WebUI/Controllers/BookingController.cs
+++ AutoBarn/AutoBarn.WebUI/Controllers/BookingController.cs
@@ -3,11 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoBarn.WebUI.Data;
+using AutoBarn.WebUI.Data.Entities;
 
 namespace AutoBarn.WebUI.Controllers
 {
     public class BookingController : Controller
     {
+        private SqlRepository<Make> _makeRepository;
+        private SqlRepository<Model> _modelRepository;
+        private SqlRepository<Service> _serviceRepository;
+
+        public BookingController()
+        {
+            _makeRepository = new SqlRepository<Make>(new AutobarnContext());
+            _modelRepository = new SqlRepository<Model>(new AutobarnContext());
+            _serviceRepository = new SqlRepository<Service>(new AutobarnContext());
+        }
+
+        public JsonResult Makes()
+        {
+            var makes = _makeRepository.GetAll();
+            return Json(makes, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Models(int id)
+        {
+            var models = _modelRepository.GetAll().Where(x => x.Make.Id == id);
+            return Json(models, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Services(int id)
+        {
+            var services = _serviceRepository.GetAll().Where(x => x.Model.Id == id);
+            return Json(services, JsonRequestBehavior.AllowGet);
+        }
+
         // GET: Booking
         public ActionResult Index()
         {
--- AutoBarn/AutoBarn.WebUI/Scripts/autobarn.js
+++ AutoBarn/AutoBarn.WebUI/Scripts/autobarn.js
@@ -9,7 +9,92 @@
 
     $(document).ready(function () {
         $('select').material_select();
+
+        $.ajax({
+            url: "/Booking/Makes",
+            type: 'GET',
+            success: function(data) {
+
+                var $select = $('#vehicleMakes');
+
+                for (var i = 0; i < data.length; i++) {
+                    console.info(data[i].Name);
+
+                    var option = $('<option>', {
+                        value: data[i].Id
+                    }).html(data[i].Name).appendTo($select);
+                }
+
+
+            },
+            error: function() {
+                console.info("There was an error downloading the makes data");
+            }
+        });
+
     });
+
+    $('#vehicleMakes').on('change', function() {
+
+        $.ajax({
+            url: "/Booking/Models/"+this.value,
+            type: 'GET',
+            success: function (data) {
+
+                var $select = $('#vehicleModels');
+                $select.empty();
+
+                $('<option disabled selected>', {
+                    value: ''
+                }).html('Choose Model').appendTo($select);
+
+                for (var i = 0; i < data.length; i++) {
+                    console.info(data[i].Name);
+
+                    var option = $('<option>', {
+                        value: data[i].Id
+                    }).html(data[i].Name).appendTo($select);
+                }
+
+
+            },
+            error: function () {
+                console.info("There was an error downloading the makes data");
+            }
+        });
+    });
+
+    $('#vehicleModels').on('change', function () {
+
+        $.ajax({
+            url: "/Booking/Services/" + this.value,
+            type: 'GET',
+            success: function (data) {
+
+                var $select = $('#services');
+                $select.empty();
+
+                $('<option disabled selected>', {
+                    value: ''
+                }).html('Choose Model').appendTo($select);
+
+                for (var i = 0; i < data.length; i++) {
+                    console.info(data[i].Name);
+
+                    var option = $('<option>', {
+                        value: data[i].Id
+                    }).html(data[i].Name).appendTo($select);
+                }
+
+
+            },
+            error: function () {
+                console.info("There was an error downloading the makes data");
+            }
+        });
+    });
+
+    
 
 
 })(jQuery); // end of jQuery name space
