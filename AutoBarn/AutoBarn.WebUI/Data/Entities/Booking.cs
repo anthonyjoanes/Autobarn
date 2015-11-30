ea20a5367b7c8a4069adad9241a242d2ec002eba
anthonyjoanes <anthonyjoanes@gmail.com>
2015-11-30 21:24:33 +0000

    maps and booking summary

--- AutoBarn/AutoBarn.WebUI/Data/Entities/Booking.cs
+++ AutoBarn/AutoBarn.WebUI/Data/Entities/Booking.cs
@@ -1,9 +1,12 @@
-﻿namespace AutoBarn.WebUI.Data.Entities
+﻿using System;
+
+namespace AutoBarn.WebUI.Data.Entities
 {
     public class Booking
     {
         public int Id { get; set; }
         public Model Model { get; set; }
         public Service Service { get; set; }
+        public DateTime Date
     }
 }
--- AutoBarn/AutoBarn.WebUI/Scripts/autobarn.js
+++ AutoBarn/AutoBarn.WebUI/Scripts/autobarn.js
@@ -4,6 +4,12 @@
         $('.button-collapse').sideNav();
         $('.parallax').parallax();
         $('.slider').slider({ full_width: true });
+
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15 // Creates a dropdown of 15 years to control year
+        });
+
     }); // end of document ready
 
 
