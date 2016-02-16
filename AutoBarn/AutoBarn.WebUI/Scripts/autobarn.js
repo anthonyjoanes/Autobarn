67eeb7df28e33f5e230a25334fc2a74d3c481dc0
anthonyjoanes <anthonyjoanes@gmail.com>
2016-02-16 20:15:03 +0000

    - fix gallery style

--- AutoBarn/AutoBarn.WebUI/Scripts/autobarn.js
+++ AutoBarn/AutoBarn.WebUI/Scripts/autobarn.js
@@ -4,7 +4,8 @@
         $('.button-collapse').sideNav();
         $('.parallax').parallax();
         $('.slider').slider({ full_width: true, indicators: false });
-
+        $('.materialboxed').materialbox();
+        $('.carousel').carousel();
         $('.datepicker').pickadate({
             selectMonths: true, // Creates a dropdown to control month
             selectYears: 15 // Creates a dropdown of 15 years to control year
