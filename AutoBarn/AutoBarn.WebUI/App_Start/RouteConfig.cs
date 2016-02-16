4e956ca43fa8094cab82745ebdf37a0c18d0f182
anthonyjoanes <anthonyjoanes@gmail.com>
2016-02-16 16:11:53 +0000

    - fix up images - include headed brands pages

--- AutoBarn/AutoBarn.WebUI/App_Start/RouteConfig.cs
+++ AutoBarn/AutoBarn.WebUI/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace AutoBarn.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Brands",
+                url: "{controller}/Brands/{heading}",
+                defaults: new { controller = "German", action = "Brands", heading = UrlParameter.Optional});
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- AutoBarn/AutoBarn.WebUI/Controllers/GermanController.cs
+++ AutoBarn/AutoBarn.WebUI/Controllers/GermanController.cs
@@ -11,9 +11,21 @@ namespace AutoBarn.WebUI.Controllers
     public class GermanController : Controller
     {
         // GET: German
-        public ActionResult Brands()
+        public ActionResult Brands(string heading = "")
         {
+            var dictionary = new Dictionary<string, string>
+            {
+                {"VW_Audi_Seat_And_Skoda_Servicing", "VW, Audi, Seat & Skoda Servicing"},
+                {"BMW_Servicing_specialists", "BMW Servicing specialists"},
+                {"Mercedes_Servicing_specialists", "Mercedes Servicing specialists"},
+                {"Motorhome_Servicing_And_Class_7_MOTs", "Motorhome Servicing & Class 7 MOT's"}
+            };
+
+
+
+
             ViewBag.CustomerReviews = MemoryCache.Default.Get("Review") as string;
+            ViewBag.BrandHeading = dictionary.ContainsKey(heading) ? dictionary[heading] : string.Empty;
             return View();
         }
     }
