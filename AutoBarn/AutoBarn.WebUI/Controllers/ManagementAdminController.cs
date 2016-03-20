eb51666d9f7e3f2c0b0bab961031637a75c02d3e
anthonyjoanes <anthonyjoanes@gmail.com>
2016-03-20 23:25:04 +0000

    missed files

---
+++ AutoBarn/AutoBarn.WebUI/Controllers/ManagementAdminController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace AutoBarn.WebUI.Controllers
+{
+    [Authorize]
+    public class ManagementAdminController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
