1f9b7e342957594be07f25bc07219b02b5f5a072
Tony Joanes <tonyjoanes@outlook.com>
2019-12-20 10:26:13 +0000

    Feature/pegasus menus (#23)
    Embedded pages for Pegasus

---
+++ AutoBarn/AutoBarn.WebUI/Controllers/PegasusController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoBarn.WebUI.Controllers
+{
+    public class PegasusController : Controller
+    {
+        // GET: Pegasus
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("Automatic-Electric-Side-Steps")]
+        public ActionResult SideSteps()
+        {
+            return View();
+        }
+
+        [ActionName("Hard-Tops")]
+        public ActionResult HardTops()
+        {
+            return View();
+        }
+
+        [ActionName("Top-Up-Covers")]
+        public ActionResult TopUpCovers()
+        {
+            return View();
+        }
+
+        [ActionName("Sports-Lids")]
+        public ActionResult SportsLids()
+        {
+            return View();
+        }
+
+        public ActionResult Accessories()
+        {
+            return View();
+        }
+    }
+}
